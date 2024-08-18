dd84515c0833abd8017e4efdff50726ac670a28d
WATUTHANTHIRIGE LASHANE YURESHKA ALWIS <133086262+Yureshka02@users.noreply.github.com>
2024-08-19 03:35:17 +0530

    code formatted

--- cypress/PageObjects/loginPage.js
+++ cypress/PageObjects/loginPage.js
@@ -5,7 +5,7 @@ class loginPage{
         txtpassword: () => cy.get('#password'),
         btnlogin: () => cy.get('.btn-primary'),
         lblLoginValidation: () => cy.get(".alert"),
-        logoutmsg: () => cy.get('#flash')
+    
 
 }
 }
